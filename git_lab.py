c70921f52f62197d754de3ea1d969170ebede708
DerekH28 <dhoward14@huskers.unl.edu>
2025-02-14 15:45:59 -0600

    mult

--- git_lab.py
+++ git_lab.py
@@ -62,4 +62,7 @@ def is_prime(n):
 
 def add_numbers(a, b):
     """Returns the sum of two numbers."""
+    return a + b
+def multiply_numbers(a, b):
+    """Returns the sum of two numbers."""
     return a * b
